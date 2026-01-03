81f27472103992ada541f94ba3febe1b917cbae2
DΛMIΛП ᄂӨPΣZ <128148139+Damloop@users.noreply.github.com>
2026-01-03 16:24:04 +0000

    Proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,67 @@
-import "bootstrap";
-import "./style.css";
+
+let quien = [
+  'Mi perro ninja',
+  'Mi abuela gamer',
+  'El cartero en patinete',
+  'Mi pájaro con ansiedad',
+  'Un vecino que no conozco',
+  'Un extraterrestre muy educado'
+];
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let accion = [
+  'se zampó',
+  'tiró por la ventana',
+  'prendió fuego sin querer',
+  'aplastó haciendo yoga',
+  'secuestró por diversión',
+  'confundió con comida'
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+let queCosa = [
+  'mi tarea',
+  'mi móvil',
+  'el coche',
+  'mi bocadillo',
+  'mi proyecto de JavaScript',
+  'mi dignidad'
+];
+
+
+let cuando = [
+  'antes de la clase',
+  'mientras estaba durmiendo',
+  'cuando estaba haciendo ejercicio (mentira)',
+  'durante mi almuerzo',
+  'mientras rezaba por aprobar',
+  'justo cuando iba a empezar a estudiar'
+];
+
+
+function numeroAleatorio(arreglo) {
+  return Math.floor(Math.random() * arreglo.length);
+}
+
+
+function generarExcusa() {
+  let parteQuien = quien[numeroAleatorio(quien)];
+  let parteAccion = accion[numeroAleatorio(accion)];
+  let parteQueCosa = queCosa[numeroAleatorio(queCosa)];
+  let parteCuando = cuando[numeroAleatorio(cuando)];
+
+  return parteQuien + " " + parteAccion + " " + parteQueCosa + " " + parteCuando;
+}
+
+// Cuando carga la página
+window.onload = function () {
+  let parrafoExcusa = document.getElementById("excuse");
+  parrafoExcusa.innerHTML = generarExcusa();
+
+  let boton = document.getElementById("btn-excuse");
+
+  boton.addEventListener("click", function () {
+    parrafoExcusa.innerHTML = generarExcusa();
+  });
 };
+
